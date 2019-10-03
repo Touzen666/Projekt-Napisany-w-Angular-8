fff56eec0bf35ae3de99e2810c1a25b43ef0ca5f
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2019-10-03 17:48:39 +0200

    Deal with Travis

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -11,7 +11,7 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   public login(login: string, password: string, on_success: any): any {
-    return this.http.post<ReturnToken>('http://backen//login', {
+    return this.http.post<ReturnToken>('http://backend/login', {
       username: login,
       password: password
     }).subscribe(function (request: ReturnToken) {
