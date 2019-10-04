4eaf2024f1bdbc76291b0bef738b5e737c91a45c
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2019-10-04 03:30:03 +0200

    Changes in build tools

--- build_tools/build_images.sh
+++ build_tools/build_images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-docker build --target backend -t touzen666/projekt-napisany-w-angular-8/backend -f dockerfiles/Dockerfile .
-docker build --target frontend -t touzen666/projekt-napisany-w-angular-8/frontend -f dockerfiles/Dockerfile .
-docker build -t touzen666/projekt-napisany-w-angular-8/mysql dockerfiles/mysql
+docker build --target backend -t touzen666/projekt-napisany-w-angular-8:backend -f dockerfiles/Dockerfile .
+docker build --target frontend -t touzen666/projekt-napisany-w-angular-8:frontend -f dockerfiles/Dockerfile .
+docker build -t touzen666/projekt-napisany-w-angular-8:mysql dockerfiles/mysql
--- build_tools/push_images.sh
+++ build_tools/push_images.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
+docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 
-docker push touzen666/projekt-napisany-w-angular-8/backend
-docker push touzen666/projekt-napisany-w-angular-8/mysql
-docker push touzen666/projekt-napisany-w-angular-8/nginx
+docker push touzen666/projekt-napisany-w-angular-8:backend
+docker push touzen666/projekt-napisany-w-angular-8:mysql
+docker push touzen666/projekt-napisany-w-angular-8:nginx
 
 
