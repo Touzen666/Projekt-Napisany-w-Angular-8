b87f02ecdc357c128350f5ff9d5b6aac267b4538
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2019-10-04 03:15:47 +0200

    At least that bunch compiles
    plus HTTP interceptors were added, to put Authorization headers where there's a need to

--- build_tools/build_images.sh
+++ build_tools/build_images.sh
@@ -3,4 +3,4 @@ set -e
 
 docker build --target backend -t touzen666/projekt-napisany-w-angular-8/backend -f dockerfiles/Dockerfile .
 docker build --target frontend -t touzen666/projekt-napisany-w-angular-8/frontend -f dockerfiles/Dockerfile .
-docker build -t touzen666/projekt-napisany-w-angular-8/mysql mysql
+docker build -t touzen666/projekt-napisany-w-angular-8/mysql dockerfiles/mysql
--- build_tools/push_images.sh
+++ build_tools/push_images.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-docker push touzen666/projekt-napisany-w-angular-8/frontend
 docker push touzen666/projekt-napisany-w-angular-8/backend
 docker push touzen666/projekt-napisany-w-angular-8/mysql
 docker push touzen666/projekt-napisany-w-angular-8/nginx
