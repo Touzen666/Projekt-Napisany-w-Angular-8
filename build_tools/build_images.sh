e5459ae3abd340400af116f94e996c6b3af3d1fa
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2019-10-04 04:32:34 +0200

    Last change for Travis to deploy that shit

--- build_tools/build_images.sh
+++ build_tools/build_images.sh
@@ -4,5 +4,3 @@ set -e
 docker build --target backend -t touzen666/projekt8:backend -f dockerfiles/Dockerfile .
 docker build --target frontend -t touzen666/projekt8:frontend -f dockerfiles/Dockerfile .
 docker build -t touzen666/projekt8:mysql dockerfiles/mysql
-
-bash build_tools/push_images.sh
